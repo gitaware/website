b17f012f513de6c36b789e8f001a32b8d6e4df3c
Jeroen <j.hermans@cloudaware.eu>
2024-02-02 23:21:00 +0000

    Updated site

--- js/privacy-manager/config.js
+++ js/privacy-manager/config.js
@@ -150,7 +150,7 @@ var klaroConfig = {
             },
         },
         nl: {
-            privacyPolicyUrl: '/privacy',
+            privacyPolicyUrl: '/privacybeleid',
             acceptAll: "Accepteer alles",
             acceptSelected: "Geselecteerde",
             close: "Sluit",
