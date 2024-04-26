a398979a4dfcd6d5a53555a4603aed6f98065ae7
Jeroen Hermans <j.hermans@cloudaware.eu>
2024-04-26 20:42:30 +0200

    Update

---
+++ static/js/cloudtr.js
@@ -0,0 +1,14 @@
+//<!-- Matomo -->
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['requireCookieConsent']);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://app.cloudaware.eu/cloudtr/";
+    _paq.push(['setTrackerUrl', u+'cloudtr']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'cloudtr.js'; s.parentNode.insertBefore(g,s);
+  })();
+//<!-- End Matomo Code -->
